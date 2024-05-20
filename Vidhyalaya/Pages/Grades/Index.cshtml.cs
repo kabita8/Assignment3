9f2af2e118c321166c5800d185473e52ebb6baa2
kabita paudel <kabitap491@gmail.com>
2024-05-20 13:51:20 +0545

    added filter

--- Vidhyalaya/Pages/Grades/Index.cshtml.cs
+++ Vidhyalaya/Pages/Grades/Index.cshtml.cs
@@ -18,10 +18,19 @@ namespace Vidhyalaya.Pages_Grades
         }
 
         public IList<Grade> Grade { get;set; } = default!;
-
+        [BindProperty(SupportsGet =true)]
+        public int? Year {get; set;}
         public async Task OnGetAsync()
         {
-            Grade = await _context.Grades.ToListAsync();
+            Console.WriteLine($"Filtering by Year: {Year}");
+            var grades = from g in _context.Grades
+                         select g;
+
+            if (Year.HasValue)
+            {
+                grades = grades.Where(g => g.SessionYear.Year == Year.Value);
+            }
+            Grade = await grades.ToListAsync();
         }
     }
 }
